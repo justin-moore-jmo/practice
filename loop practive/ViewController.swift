4b8db91fbd14e076727ec05bd08014f6016e02eb
justin moore <justin.moore.jmo@gmail.com>
2019-04-09 14:01:21 -0500

    Initial Commit

---
+++ loop practive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loop practive
+//
+//  Created by Justin Moore on 4/9/19.
+//  Copyright © 2019 Justin Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
