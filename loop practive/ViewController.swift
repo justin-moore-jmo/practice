162abc551a96098715212fbb77cefa97f211ca8c
justin moore <justin.moore.jmo@gmail.com>
2019-04-16 14:03:00 -0500

    first push

--- loop practive/ViewController.swift
+++ loop practive/ViewController.swift
@@ -13,8 +13,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switchLabel.text = "I love this button"
+        sliderLabel.text = String(Int(sliderOutlet.value))
+        stepperLabel.text = String(Int(stepperOutlet.value))
     }
 
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var switchOutlet: UISwitch!
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBOutlet weak var stepperOutlet: UIStepper!
+    
 
+    @IBAction func switchButton(_ sender: Any) {
+        if switchOutlet.isOn == true {
+            switchLabel.text = "Switch on"
+        } else {
+            switchLabel.text = "Switch off"
+        }
+    }
+    
+    @IBAction func sliderButton(_ sender: Any) {
+        sliderLabel.text = String(Int(sliderOutlet.value))
+    }
+    
+    @IBAction func stepperButton(_ sender: Any) {
+        stepperLabel.text = String(Int(stepperOutlet.value))
+        var stepNumber = Int(stepperOutlet.value)
+        if stepNumber == 20 {
+            for usefulNumber in 1...20{
+                stepNumber = 20 - usefulNumber
+                stepperOutlet.value = Double(stepNumber)
+                stepperLabel.text = String(Int(stepperOutlet.value))
+            }
+            
+        }
+    }
+    
+    @IBAction func defaultButton(_ sender: Any) {
+        switchOutlet.isOn = true
+        sliderOutlet.value = 5.0
+        stepperOutlet.value = 0
+        switchLabel.text = "Switch on"
+        sliderLabel.text = String(Int(sliderOutlet.value))
+        stepperLabel.text = String(Int(stepperOutlet.value))
+    }
+    
+    @IBAction func syncButton(_ sender: Any) {
+        if stepperOutlet.value >= 10{
+        sliderOutlet.maximumValue = Float (stepperOutlet.value)
+        }
+        
+        sliderOutlet.value = Float(stepperOutlet.value)
+        sliderLabel.text = String(Int(sliderOutlet.value))
+        
+        
+    }
 }
 
